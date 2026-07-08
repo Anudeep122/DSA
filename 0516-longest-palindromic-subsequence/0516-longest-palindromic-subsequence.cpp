5712ab8a587d4039c39e8fbf5c6c1bdbb5980cfd
Anudeep122 <anudeepvalmiki1914@gmail.com>
2026-07-08 12:45:18 +0530

    Time: 71 ms (58.14%), Space: 76.2 MB (7.53%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -12,17 +12,18 @@ public:
         reverse(str.begin(),str.end());
         int n=str.length();
         vector<vector<int>> dp(n+1,vector<int> (n+1,0));
-        for(int i=0;i<=n;i++) dp[0][i]=0;
-        for(int i=0;i<=n;i++) dp[i][0]=0;
+        vector<int> prev(n+1,0),curr(n+1,0);
+        for(int i=0;i<=n;i++) prev[i]=0;
 
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n;j++){
                 if(s[i-1]==str[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
+                    curr[j]=1+prev[j-1];
                 }
-                else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                else curr[j]=max(prev[j],curr[j-1]);
             }
+            prev=curr;
         }
-        return dp[n][n];
+        return prev[n];
     }
 };
