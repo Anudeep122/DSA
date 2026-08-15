6fbd89305500131de3356c72929d05cf89630eb1
Anudeep122 <anudeepvalmiki1914@gmail.com>
2026-08-15 21:03:32 +0530

    Time: 0 ms (100%), Space: 18.1 MB (46.96%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,22 +1,32 @@
 class Solution {
 public:
-    bool search(vector<int>& nums, int k) {
-        int low=0,high=nums.size()-1;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(nums[mid]==k) return true;
-            if(nums[mid]==nums[low] && nums[mid]==nums[high]){
-                low++;high--;continue;
-            }
-            else if(nums[low]<=nums[mid]){
-                if(nums[low]<=k && k<=nums[mid]) high=mid-1;
-                else low=mid+1;
-            }
-            else{
-                if(nums[mid]<=k && k<=nums[high]) low=mid+1;
-                else high=mid-1;
+    bool search(vector<int>& v, int k) {
+        int n=v.size();
+        int lo=0,hi=n-1,ans=0;
+        while(lo<hi && v[lo]==v[hi])lo++;
+        int x = lo;
+        while(lo<=hi){
+            int mid = (lo+hi)/2;
+            if(v[mid]<v[x])hi = mid-1;
+            else {
+                ans = mid;
+                lo=mid+1;
             }
         }
+        
+        if(ans==n-1 && x!=0)ans = x-1;
+        
+        lo=0,hi=n-1;
+        if(k>=v[0] && k<=v[ans])hi=ans;
+        else lo = ans+1;
+
+        while(lo<=hi){
+            int mid = (lo+hi)/2;
+            if(v[mid]==k)return true;
+            else if(v[mid]<k)lo=mid+1;
+            else hi = mid-1;
+        }
+
         return false;
     }
 };
