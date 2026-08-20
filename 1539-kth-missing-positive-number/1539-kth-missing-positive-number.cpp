d79dd3f32943106d611703c821dbbc7f91b0b5bd
Anudeep122 <anudeepvalmiki1914@gmail.com>
2026-08-20 23:33:52 +0530

    Time: 0 ms (100%), Space: 13.2 MB (92.84%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,43 +1,23 @@
 class Solution {
 public:
-    // bool checker(vector<int>& arr,int k,int mid){
-    //     int n=arr.size();
-    //     auto it=lower_bound(arr.begin(),arr.end(),mid);
-    //     int index=it-arr.begin();int val=*it;
-    //     if(val-index<k) return true;
-    //     else return false;
-    // }
-    bool find(vector<int>& arr,int el){
-        int low=0,high=arr.size()-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(arr[mid]==el) return true;
-            else if(arr[mid]<el) low=mid+1;
-            else high=mid-1;
-        }
-        return false;
-    }
-
     int findKthPositive(vector<int>& arr, int k) {
-        int n=arr.size();
-        int low=1,high=*max_element(arr.begin(),arr.end())+k;
-        // int ans=0;
-        // while(low<=high){
-        //     int mid=low+(high-low)/2;
-        //     if(checker(arr,k,mid)){
-        //         ans=mid;
-        //         low=mid+1;
-        //     }
-        //     else high=mid-1;
-        // }
-        // return ans;
-        vector<int> miss;
-        for(int i=1;i<=high;i++){
-            if(find(arr,i)) continue;
-            else{
-                miss.push_back(i);
+        int n = arr.size();
+        arr.insert(arr.begin(),0);
+        int lo=1,hi=n;
+        if(k<arr[1])return k;
+
+        int ans = 0,rem = 0;
+        while(lo<=hi){
+            int mid = (lo+hi)/2;
+            int req = arr[mid]-mid;
+            if(req<k){
+                ans = mid;
+                rem = k-req;
+                lo = mid+1;
             }
+            else hi = mid-1;
         }
-        return miss[k-1];
+
+        return arr[ans]+rem;
     }
 };
