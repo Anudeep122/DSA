c1d4e3a7495803f199174e1aceac5324f06cad32
Anudeep122 <anudeepvalmiki1914@gmail.com>
2026-08-22 20:26:02 +0530

    Time: 0 ms (100%), Space: 13.2 MB (15.45%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
-    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& arr) {
         int n=nums1.size();
-        int m=nums2.size();
-        
-        vector<int> ans(n,-1);
-        for(int i=0;i<n;i++){
-            int flag=0;
-            for(int j=0;j<m;j++){
-                if(nums1[i]==nums2[j]){
-                    flag=1;
-                }
-                if(flag && nums2[j]>nums1[i]){
-                    ans[i]=nums2[j];
-                    break;
-                }
+        int m=arr.size();
+        stack<int> st;
+        vector<int> ans(m,-1);
+        for(int i=0;i<m;i++){
+            while(!st.empty() && arr[st.top()]<arr[i]){
+                ans[st.top()]=arr[i];
+                st.pop();
             }
+            st.push(i);
         }
-        return ans;
+
+        vector<int> ind(10005,0);
+        for(int i=0;i<m;i++){
+            ind[arr[i]] = i;
+        }
+        vector<int> ans2(n,-1);
+
+        for(int i=0;i<n;i++){
+            ans2[i] = ans[ind[nums1[i]]];
+        }
+
+        return ans2;
     }
 };
