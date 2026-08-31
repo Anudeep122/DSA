f6f66bb05828128384433b52d62592e1eb7bba0b
Anudeep122 <anudeepvalmiki1914@gmail.com>
2026-09-01 04:13:00 +0530

    Time: 68 ms (68.7%), Space: 193.5 MB (35.21%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -2,36 +2,38 @@ class Solution {
 public:
     int sumSubarrayMins(vector<int>& v) {
         int n=v.size();
-
+        int mod=1e9+7;
+        //next minimum
         stack<int> st;
-        vector<int> prev(n+1,-1),nxt(n+1,n);
-        for(int i=0;i<n;i++){
-            while(!st.empty() && v[st.top()]>v[i]){
-                nxt[st.top()] = i;
+        vector<int> nm(n,n);
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && v[st.top()]>v[i] ){
                 st.pop();
             }
-            
+            if(st.empty()) nm[i]=n;
+            else nm[i]=st.top();
             st.push(i);
         }
-
+        
+        //prev minimum
         stack<int> st2;
-        for(int i=n-1;i>=0;i--){
-            while(!st2.empty() && v[st2.top()]>=v[i]){
-                prev[st2.top()] = i;
+        vector<int> pm(n,-1);
+        for(int i=0;i<n;i++){
+            while(!st2.empty() && v[st2.top()]>=v[i] ){
                 st2.pop();
             }
+            if(st2.empty()) pm[i]=-1;
+            else pm[i]=st2.top();
             st2.push(i);
         }
 
-        long long ans = 0;
-        long long mod = 1e9+7;
+        int ans=0;
         for(int i=0;i<n;i++){
-            long long x = i-prev[i];
-            long long y = nxt[i]-i;
-            long long z = (x*y)%mod;
-            ans = (ans + (z*v[i])%mod)%mod;
+            int l=i-pm[i];
+            int r=nm[i]-i;
+            ans=(ans+ (l*r*1LL*v[i])%mod)%mod ;
         }
-
-        return (long)ans;
+        return ans;
+        
     }
 };
